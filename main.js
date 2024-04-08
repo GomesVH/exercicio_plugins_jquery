c33e90678838f630d21ffa151b6441dd6dcc4e2f
Victor Gomes <victorgomeslucio1@hoymail.com>
2024-04-08 16:17:25 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function()         
+        {                                                    
+
+            $('#telefone').mask('(00) 00000-0000',    
+            {
+                placeholder: '(11) 99880-7584'  
+            })                                        
+            
+            $('#cpf').mask('000.000.000-00',    
+            {
+                placeholder: '000.000.000-00'                                      
+            })  
+            ''
+            $('#cep').mask('00000-000',    
+            {
+                placeholder: '00000-000'                                      
+            })            
+        })
